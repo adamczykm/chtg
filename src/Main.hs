3171e6529a703f033504380fa063b9d26f28fb64
anks <michal.anks@gmail.com>
2015-05-19 03:59:03 +0200

    implemented & not tested

--- src/Main.hs
+++ src/Main.hs
@@ -1,161 +1,307 @@
 module Main where
 
-import Control.Monad (liftM)
+import System.Environment
+import Control.Monad (guard)
+
 import Data.Graph.Inductive
-import qualified Data.IntMap as IM
-import Data.Maybe (fromJust)
+import qualified Data.Set as S
 import Data.Set (fromList, toList)
-import Data.List 
+import qualified Data.IntMap as IM
+import Data.List (partition,sortBy,minimumBy, maximumBy)
 import Data.Function (on)
-import qualified Data.PSQueue as PQ
 
+import qualified Debug.Trace as T
 
---------------------------------- definicje typow ---------------------------------------
+-- =================== Zdefiniowane typy
 
 -- | Typ naszego grafu
-type Graf = Gr Name ()
+type Graf = Gr (Maybe Color) ()
 
--- | Pokolorowany graf
-type ColoredGraf = Gr (Name, Color) ()
+-- | Graf pokolorowany
+type ColoredGraf = Gr (Color) ()
 
 -- | Kolor wierzcholka
-newtype Color = Color Int
-              deriving (Eq, Show, Read, Ord)
+type Color = Int
 
--- | Nazwa wierzcholka
-type Name = Int
+-- | Funkcja wybierajaca kolejnosc
+type Order = Graf -> [Node]
 
--- | Kolorowanie to funkcja, ktora wierzcholkowi daje kolor
-type Coloring = Name -> Maybe Color
-
--- | Algorytm dostaje graf i mowi na ile najmniej kolorow mozna pomalowac
--- aby kazda para kolorow indukowala wierzcholek bez sciezek dlugosci 3
--- oraz zwraca odpowiednie kolorowanie
-type Algorithm = Graf -> (Int, Coloring)
+-- | Algorytm 
+type Algorithm = Graf -> ColoredGraf
 
 
--------------------------------- parsowanie grafu ----------------------------------
+-- =================== Kolejności brania wierzchołków
 
-parseGraphFromFileWith :: (String -> Graf) -> FilePath -> IO (Graf)
-parseGraphFromFileWith parseFun fp = do
-  gstr <- readFile fp
-  return $ parseFun gstr
+-- | Largest First
+lfOrder :: Order
+lfOrder g = reverse . sortBy (compare `on` deg g) $ nodes g
 
-_graphFromNeighbourMatrix :: (String -> Graf)
-_graphFromNeighbourMatrix = fromMatrix . readMatrix
+-- | Smallest Last
+slOrder :: Order
+slOrder g' = reverse $ map snd $ slWorker [] g'
+  where slWorker :: [(Int, Node)] -> Graf -> [(Int, Node)]
+        slWorker xs g | g == empty = xs
+        slWorker xs g              = slWorker ((deg g sm, sm):xs)
+                                              (delNode sm g)
+          where sm = minimumBy (compare `on` (deg g)) (nodes g)
+
+-- | DSatur
+dsOrder :: Order
+dsOrder g = if ns' == []
+            then []
+            else reverse $ go initialSatur []
+
+  where go :: IM.IntMap Int -> [Node] -> [Node]
+        go ds ord = case viewMaxValue ds of
+                     Nothing       -> ord
+                     Just (k, nds) -> go (updateDs (safeNeighbors g k) nds)
+                                         (k:ord)
+
+          where updateDs xs nds = foldr (IM.update (Just . succ)) nds xs
+                viewMaxValue im | im == IM.empty = Nothing
+                viewMaxValue im                  = Just (fst m, rest)
+                  where m    = maximumBy (compare `on` snd) $ IM.toList im
+                        rest = IM.delete (fst m) im
+
+        ns' = lfOrder g
+        initialSatur = IM.fromList $ zip ns' (1 : repeat 0)
+
+-- =================== Algorym kolorowania
+
+
+updateMany :: (a->a) ->  IM.IntMap a -> [IM.Key] -> IM.IntMap a
+updateMany f  = foldr (IM.update (Just . f)) 
+
+-- | Funkcja dla danej kolejnosci wybierania wierzcholkow z grafu
+--   zwraca poprawnie (zgodnie z warunkami zadania) pokolorwany graf
+colorGraphWithOrder :: Order -> Graf -> ColoredGraf
+colorGraphWithOrder order g = go (order g) empty
+  where go :: [Node] -> ColoredGraf -> ColoredGraf
+        go []     cg = cg
+        go (n:ns) cg = go ns (withNodeColored cg
+                                              n
+                                              newcolor)
+          where newcolor = pickColor cg n
+
+
+        -- | Zwraca graf z wierzcholkiem pomalowanym na dany kolor
+        withNodeColored :: ColoredGraf -> Node -> Color -> ColoredGraf
+        withNodeColored cg n c = case match n g of
+          (Just (p, _, _, s), _) -> (f p, n, c,f s) & cg
+            where f = filter (\(_,n') -> n' `gelem` cg)
+          (Nothing,           _) -> error "not existent node!?"
+
+
+        -- | Wybiera najmniejszy możliwy dla wierzchołka kolor
+        --   zgodny z warunkami zadania.
+        pickColor :: ColoredGraf -> Node -> Color
+        pickColor cg n = head $ filter (tryColor cg n) [1..maxColor]
+          where maxColor = case safeOnEmpty maximum $ toList (colors cg) of
+                            Nothing -> 1
+                            Just c  -> c + 1
+
+        -- | Zwraca czy pokolorowanie wierzcholka na dany kolor spełnia
+        --   warunki zadania.
+        tryColor :: ColoredGraf -> Node -> Color -> Bool
+        tryColor cg n c = case IM.lookup c colorsCount of
+            Just _  -> False
+            -- podzial na dwie klasy kolorów:
+            -- scn - co najwyzej jeden sasiad n w tym kolorze
+            -- mcn - wiecej niz jeden sasiad n w tym kolorze
+            Nothing -> let (scn, mcn) =
+                            partition ((==Just 1) .
+                                       (flip IM.lookup) colorsCount .
+                                       color cg)
+                                      neighs
+                      in not (startsPath scn) && not (inPath mcn)
+
+          where -- | czy stanie się początkiem scieżki o dl. 3
+                startsPath scn = some $ do
+                  n1 <- scn
+                  n2 <- safeNeighbors cg n1
+                  guard (n2 /= n && color cg n2 == c)
+                  n3 <- safeNeighbors cg n2
+                  guard (n3 /= n1 && color cg n3 == color cg n1)
+
+                -- | Czy bedzie czescią scieżki o dl. 3?
+                inPath mcn = some $  do
+                  n1 <- mcn
+                  n2 <- safeNeighbors cg n1
+                  guard (n2 /= n && color cg n2 == c)
+
+                -- | Sasiedzi wierzchołka n, w pokolorowanej czesci grafu.
+                neighs = filter (\x -> x `gelem` cg) (safeNeighbors g n)
+
+                -- | Pomocniczy slownik ilosci kolorów
+                colorsCount = foldr inc IM.empty (map (color cg) neighs)
+                  where inc c' ccs = case IM.lookup c' ccs of
+                                     Nothing -> IM.insert c' (1 :: Integer) ccs
+                                     Just i  -> IM.insert c' (i + 1)        ccs
+
+printAnswer :: ColoredGraf -> String
+printAnswer cg = "Ilość kolorów: " ++ show (maximum $ toList (colors cg))
+                 ++ "\nKolorowanie: " ++ show (labNodes cg)
+
+-- =================== Funcje pomocnicze
+
+-- graphInducedByVertices :: gr a b -> [Node] -> gr a b
+-- graphInducedByVertices g ns = mkGraph ns ledges
+--   where ledges = efilter (\(v1,v2) -> v1 `elem` ns &&
+--                                       v2 `elem` ns) ns
+
+-- | Zwraca kolor wierzcholka w grafie g
+color :: ColoredGraf -> Node -> Color
+color g n = case lab g n of
+           Just c  -> c
+           Nothing -> error $ show g
+
+-- | Zwraca wszystkie kolory w grafie
+colors :: ColoredGraf -> S.Set Color
+colors cg | cg == empty = S.empty
+colors cg               = fromList $ map (color cg) (nodes cg)
+
+-- | Zwraca sasiadow wierzcholka w grafie
+safeNeighbors :: (Graph gr, Eq (gr a b)) => gr a b -> Node -> [Node]
+safeNeighbors g _ | g == empty = []
+safeNeighbors g n              = neighbors g n
+
+-- | Prawda <=> pusta lista
+none :: [a] -> Bool
+none [] = True
+none _  = False
+
+-- | Prawda <=> niepusta lista
+some :: [a] -> Bool
+some = not . none
+
+-- | Zwraca Nothing zamiast rzucac wyjatki
+safeOnEmpty :: ([a] -> b) -> [a] -> Maybe b
+safeOnEmpty _ [] = Nothing
+safeOnEmpty f xs = Just $ f xs
+
+-- | Usuwa duplikaty z listy
+unique :: Ord a => [a] -> [a]
+unique = toList . fromList
+
+-- | Konwersja Int -> Bool
+fromInt :: Int -> Bool
+fromInt 0 = False
+fromInt _ = True
+
+graphSize :: Graf -> Int
+graphSize = length . nodes
+
+dbg :: Show a => String -> a -> a
+dbg s a = T.traceStack (s ++ ": " ++ (show a) ++ "\n") a
+
+-- ================== Funkcje testujące
+
+-- | Sprawdza czy graf g posiada sciezke o dlugosci n.
+hasPathWithLengthN :: Int -> Graf -> Bool
+hasPathWithLengthN n g = any (== n) pathLengths
+  where pathLengths = concat $ (map . map) snd dists
+        dists       = map (\x -> level x (undir g)) $
+                          map head (components g)
+
+checkColoring :: ColoredGraf -> Bool
+checkColoring cg = (not $ any hasSameColoredNeighbour (nodes cg))
+                   &&
+                   (not $ any colorsInducePath everyPairOfColors)
+  where hasSameColoredNeighbour n = col n `elem` map col
+                                                     (neighbors cg n)
+
+        colorsInducePath = undefined
+
+        everyPairOfColors = let cs = toList $ colors cg
+                            in [(c1,c2) | c1 <- cs, c2 <- cs, c1 /= c2]
+
+        
+                               
+        col = color cg
+
+
+-- -- | Ten test sprawdza poprawnosc kolorowania grafu ( czy spelnia warunek),
+-- --   Nie sprawdza czy znalezione rozwiazanie jest najmniejsze.
+-- test :: Algorithm -> Graf -> ColoredGraf
+-- test alg g = not $ any id $ map (hasPathWithLengthN 3) inducedGraphs
+--   where (_, col) = alg g
+--         inducedGraphs = map (_graphInducedByColors g col)
+--                             (everyPairOfColors _colors)
+--         _colors = unique $ map ( fromJust . col) (nodes g)
+
+
+
+-- ================== Funkcje parsujące
+
+-- | Convert list of lists of neighbours to graf
+fromNeighbourhoodList :: [[Node]] -> Graf
+fromNeighbourhoodList nss = fixGraph $ mkGraph (zip names (repeat Nothing)) ledges
+  where names = [1..(length nss)]
+        
+        ledges = concat $ map (uncurry edgesFromNs) (zip names nss)
+        
+        edgesFromNs n = map (\x -> (n, x, ()))
+        
+        fixGraph g' = undirG $ clearSelfEdges $ addMissingNodes g' (nodesFromEdges g')
+        
+          where nodesFromEdges = unique .
+                                 foldl (\xs (v1,v2) -> v1 : v2 : xs) [] .
+                                 edges
+
+                clearSelfEdges g = efilter (\(v1,v2, _) -> v1 /= v2) g
+
+                undirG g = foldr delEdge (undir g)
+                                 [(v1,v2) |
+                                  v1 <- [1..(graphSize g)],
+                                  v2 <- [(v1+1)..(graphSize g)]]
+
+                addMissingNodes g []     = g
+                addMissingNodes g (n:ns) | notElem n (nodes g)
+                                         = addMissingNodes (insNode (n,Nothing) g) ns
+                addMissingNodes g (_:ns) = addMissingNodes g ns
+
+
+parseFromNeighbourMatrix :: String -> Graf
+parseFromNeighbourMatrix = fromMatrix . readMatrix
   where readMatrix :: String -> [[Bool]]
         readMatrix = (map . map) (fromInt . read) . map words . lines
 
         fromMatrix :: [[Bool]] -> Graf
         fromMatrix = fromNeighbourhoodList . map neighbours
-          where neighbours = (map fst) . filter ((== True) . snd) . zip ([1..] :: [Name])
+          where neighbours = (map fst) .
+                             filter ((== True) . snd) .
+                             zip [1..]
 
-_graphFromNeighbourList :: (String -> Graf)
-_graphFromNeighbourList = fromNeighbourhoodList . readNeighbourhoodList
-         where readNeighbourhoodList :: String -> [[Name]]
+readFromMatrixFile :: FilePath -> IO Graf
+readFromMatrixFile = parseGraphFromFileWith parseFromNeighbourMatrix
+
+
+parseGraphFromFileWith :: (String -> Graf) -> FilePath -> IO Graf
+parseGraphFromFileWith parseFun fp = do
+  gstr <- readFile fp
+  return $ parseFun gstr
+
+graphFromNeighbourList :: String -> Graf
+graphFromNeighbourList = fromNeighbourhoodList . readNeighbourhoodList
+         where readNeighbourhoodList :: String -> [[Node]]
                readNeighbourhoodList = (map . map) read  . map words . lines 
 
-fromNeighbourhoodList :: [[Name]] -> Graf
-fromNeighbourhoodList nss = undir . fixGraph $ mkGraph (zip names names) ledges
-  where names = [1..(length nss)]
-        ledges = concat $ map (uncurry edgesFromNs)  (zip names nss)
-        edgesFromNs n = map (\x -> (n, x, ()))
 
-        fixGraph g' = addMissingNodes g' (nodesFromEdges g')
-          where nodesFromEdges :: Graf -> [Node]
-                nodesFromEdges = unique . foldl (\xs (v1,v2) -> v1 : v2 : xs) [] . edges
-
-                addMissingNodes g [] = g
-                addMissingNodes g (n:ns) | notElem n (nodes g) = addMissingNodes (insNode (n,n) g) ns
-                addMissingNodes g (_:ns) = addMissingNodes g ns
-
-   
-_parseGraph :: String -> Graf
-_parseGraph = read
--- ======================     algorytmy     ========================
-
-lfOrder :: Graf -> [(Int, Name)]
-lfOrder g = reverse . sort $ map (\n -> (deg g n, _label g n)) (nodes g)
-
-slOrder :: Graf -> [(Int, Name)]
-slOrder g' = reverse $ slWorker [] g'
-  where slWorker :: [(Int, Name)] -> Graf -> [(Int, Name)]
-        slWorker xs g | g == empty = xs
-        slWorker xs g                    = slWorker ((deg g sm, _label g sm):xs) (delNode sm g)
-          where sm = minimumBy (compare `on` (deg g)) (nodes g)
-
-dsOrder :: Graf -> [(Int, Name)]
-dsOrder = undefined
-
-coloring :: [Name] -> Coloring
-coloring ns = flip IM.lookup  . IM.fromList $ zip ns $ map Color [1..]
------------------------------------------------- testing algorithms ---------------------------------------------
-
-oneColor :: Algorithm
-oneColor _ = (1, const $ Just (Color 1))
-
-dummyAlgorithm :: Algorithm
-dummyAlgorithm g = (length $ nodes g, Just . Color)
-
-everyPairOfColors :: [Color] -> [[Color]]
-everyPairOfColors cs = singleColors ++ pairsOfColors
-  where singleColors = map (:[]) cs
-        pairsOfColors = [[c1,c2] | c1 <- cs, c2 <- cs, c1 /= c2]
-
--- | Rzuci wyjatek jezeli kolorowanie jest nie zdefiniowane
---  Dla ktoregos z wierzcholkow
-_graphInducedByColors :: Graf -> Coloring -> [Color] -> Graf
-_graphInducedByColors g col cs = mkGraph leftV leftE
-  where color =  fromJust . col . snd
-        leftV = filter (\n -> color n `elem` cs) (labNodes g)
-        uleftV = map fst leftV
-        leftE = labEdges $ efilter (\(v1, v2, _) -> (v1 `elem` uleftV) && (v2 `elem` uleftV))  g
-
-hasPathWithLengthN :: Int -> Graf -> Bool
-hasPathWithLengthN n g = any (== n) pathLengths
-  where pathLengths = concat $ (map . map) snd dists
-        dists               = map (\x -> level x  (undir g))  $ map head (components g)
-
--- | Ten test sprawdza poprawnosc kolorowania grafu ( czy spelnia warunek),
---   Nie sprawdza czy znalezione rozwiazanie jest najmniejsze.
-test :: Algorithm -> Graf -> Bool
-test alg g = not $ any id $ map (hasPathWithLengthN 3) inducedGraphs
-  where (_, col) = alg g
-        inducedGraphs = map (_graphInducedByColors g col) (everyPairOfColors _colors)
-        _colors = unique $ map ( fromJust . col) (nodes g)
-
----------------------------------------------- pomocnicze funkcje -----------------------------------------
-
--- | uwazac tylko z pewnoscia ze node ma labela
-_label :: Graf -> Node -> Name
-_label g n = case lab g n of
-  Nothing -> error "Node bez labela"
-  Just name -> name
-
-fromInt :: Int -> Bool
-fromInt 0 = False
-fromInt _ = True
-
-unique :: Ord a => [a] -> [a]
-unique = toList . fromList
-
-getGraphFromFile :: String -> IO (Graf)
-getGraphFromFile str = do
-  g <- parseGraphFromFileWith _graphFromNeighbourMatrix str
-  return g
-
-readFromKeyboard :: IO (Graf)
-readFromKeyboard = liftM _graphFromNeighbourList $ input ""
-  where input str = do
-          line <- getLine
-          if line /= "" then input (str  ++ line ++ "\n") else do
-            print str
-            return str
+-- ======================== Main
 
 main :: IO ()
 main = do
-  g <- parseGraphFromFileWith _graphFromNeighbourMatrix "src/matrix_path3"
-  print g
-  print $ test dummyAlgorithm g
-  print $ test oneColor g
+  g  <- getArgs   >>= readFromMatrixFile . (!! 0)
 
+  putStrLn "== Largest First =="
+  putStrLn $ printAnswer $ colorGraphWithOrder lfOrder g
+
+  putStrLn "== Smallest Last =="
+  putStrLn $ printAnswer $ colorGraphWithOrder slOrder g
+
+  putStrLn "== DSatur        =="
+  putStrLn $ printAnswer $ colorGraphWithOrder dsOrder g
+
+  return ()
+
+  
